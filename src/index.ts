20b1cf85cf6861d33685c703dce3fd1dcaa87230
Truong.Nat <87919564+truongnat@users.noreply.github.com>
2023-12-19 07:32:37 +0700

    feat: init

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,17 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
+import { swagger } from "@elysiajs/swagger";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .use(swagger())
+  .get("/id/:id", ({ params: { id } }) => id, {
+    params: t.Object({
+      id: t.Numeric(),
+    }),
+  })
+  .listen(8080);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 );
+
+export type App = typeof app;
